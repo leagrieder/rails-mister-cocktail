b28203b162df5ecb92b0c879d74df74ab0d00b0a
Lea Grieder <lea@grieder.org>
2020-05-22 12:22:55 +0200

    mister cocktail mvc and crud working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+Ingredient.create(name: 'lime')
+Ingredient.create(name: 'rasberry')
+Ingredient.create(name: 'blueberry')
+Ingredient.create(name: 'Vodka')
+Ingredient.create(name: 'Rum')
+Ingredient.create(name: 'Tequila')
+
+Cocktail.destroy_all
+
+Dose.destroy_all
+
